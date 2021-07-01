a5f4bfc51e00fe2d1242800b4649f64121a8a6d6
Yuziyue <ziyue.yu@ai-hit.com>
2021-07-01 09:44:31 +0800

    Read/Write from Arduino(ESP32) , via USB/OTG

---
+++ app/src/main/java/com/example/myesp/MainActivity.java
@@ -0,0 +1,185 @@
+package com.example.myesp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hoho.android.usbserial.driver.CdcAcmSerialDriver;
+import com.hoho.android.usbserial.driver.ProbeTable;
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button sendButton;
+    private Button connectButton;
+    private Button closeButton;
+    private Button clearButton;
+    private TextView consoleView;
+    private ProgressBar progressBar;
+    private EditText editText;
+
+    UsbSerialPort port;
+
+
+    private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
+    private SerialInputOutputManager mSerialIoManager;
+
+
+
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    //请求状态码
+    private static int REQUEST_PERMISSION_CODE = 1;
+
+
+
+    private final  SerialInputOutputManager.Listener mListener =
+            new SerialInputOutputManager.Listener() {
+                @Override
+                public void onRunError(Exception e) {
+                    Log.d("TAG", "Runner stopped.");
+                }
+
+                @Override
+                public void onNewData(final byte[] data) {
+                    //TODO 新的数据
+
+                    runOnUiThread(()->{output(new String(data));});
+                }
+            };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //Request Permissions
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_PERMISSION_CODE);
+            }
+        }
+
+
+        sendButton = findViewById(R.id.sendButton);
+        connectButton = findViewById(R.id.connectButton);
+        closeButton = findViewById(R.id.closeButton);
+        clearButton = findViewById(R.id.clearButton);
+        consoleView = findViewById(R.id.consoleText);
+        progressBar = findViewById(R.id.progressBar);
+        editText = findViewById(R.id.send_Text);
+
+        progressBar.setVisibility(View.INVISIBLE);
+
+        connectButton.setOnClickListener(v->{
+            // Find all available drivers from attached devices.
+            UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+            List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+            if (availableDrivers.isEmpty()) {
+                return;
+            }
+
+            // Open a connection to the first available driver.
+            UsbSerialDriver driver = availableDrivers.get(0);
+            UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+            if (connection == null) {
+                // add UsbManager.requestPermission(driver.getDevice(), ..) handling here
+                Toast.makeText(this, "Permission Error", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            port = driver.getPorts().get(0); // Most devices have just one port (port 0)
+            try {
+                port.open(connection);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if(port.isOpen()){
+                progressBar.setVisibility(View.VISIBLE);
+                output("Set port Success!");
+
+                mSerialIoManager = new SerialInputOutputManager(port, mListener);//添加监听
+                mSerialIoManager.start();
+            }
+        });
+
+        sendButton.setOnClickListener(v->{
+            String strSend = editText.getText().toString()+'\n';
+//            String strSend = "hello\n";
+            try {
+                port.write(strSend.getBytes(),1000);
+                Toast.makeText(this, "Write Successful", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+//            byte buffer[] = new byte[16];
+//            int numBytesRead = 0;
+//            try {
+//                numBytesRead = port.read(buffer, 1000);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//          //  Log.d("TAG", "Read " + numBytesRead + " bytes.");
+//            Toast.makeText(this, ""+numBytesRead, Toast.LENGTH_SHORT).show();
+
+        });
+
+        closeButton.setOnClickListener(v->{
+            try {
+                port.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if(!port.isOpen()){
+                output("Close Port!");
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        clearButton.setOnClickListener(v->{
+            consoleView.setText("");
+        });
+    }
+
+    public void output(String content) {
+        consoleView.append("> " + content + "\n");
+    }
+
+
+
+
+
+}
+
